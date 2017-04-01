e83f975e132e37402858aca93c9bb6a206c5f24f
Brett Sheleski <brett@sheleski.com>
2017-03-31 22:51:56 -0500

    got Collatz working

--- Collatz/collatz.js
+++ Collatz/collatz.js
@@ -8,6 +8,37 @@ Collatz = (function (createjs) {
         self.options = options || {};
         self.stage = new createjs.Stage(canvas);
 
+        self.container = new createjs.Container();
+
+        var p = null;
+
+        self.container.on("mousedown", function(evt){
+            p = {x : evt.stageX, y : evt.stageY};
+        })
+
+        self.container.on("pressmove", function(evt){
+         
+            self.container.x += (evt.stageX - p.x) / self.container.scaleX;
+            self.container.y += (evt.stageY - p.y) / self.container.scaleY;
+
+            self.stage.update();
+
+            p = {x : evt.stageX, y : evt.stageY};
+        });
+
+        self.container.on("dblclick", function(evt){
+
+            self.container.regX = evt.stageX / self.container.scaleX;
+            self.container.regY = evt.stageY / self.container.scaleY;
+
+            self.container.scaleX *= 1.1;
+            self.container.scaleY *= 1.1;
+
+            self.stage.update();
+        })
+
+        self.stage.addChild(self.container);
+
 
         if (typeof self.options.width === 'undefined')
             self.options.width = this.defaultOptions.width;
@@ -74,23 +105,40 @@ Collatz = (function (createjs) {
     p.createShape = function () {
         var shape = new createjs.Shape();
         shape.graphics.setStrokeStyle(this.options.width, "round", "round");
-        shape.graphics.beginStroke(randomColor());
+        shape.graphics.beginStroke("red");
 
-        this.stage.addChild(shape);
+        this.container.addChildAt(shape, 0);
 
         return shape;
     }
 
     p.draw = function (n) {
 
+
+        this.container.removeAllChildren();
+
+        var nodes = [];
+        var allNodes = {};
+
+        for(var i = 0 ; i < n; ++i){
+            nodes[i] = new Node(i + 1);
+            allNodes[(i + 1).toString()] = nodes[i];
+        }
+
+        for(var i = 0 ; i < n; ++i){
+            nodes[i].calculate(nodes, allNodes);
+        }
+
         var p = { x: 0, y: 100, angle: 0 };
 
         var shape = this.createShape();
         shape.graphics.moveTo(p.x, p.y);
 
-        drawPoint(shape, p, 1, n, this);
+        //drawPoint(shape, p, 1, n, this);
+
+        nodes[0].draw(shape, p, this);
         
-        this.stage.addChild(shape);
+        this.container.addChild(shape);
 
 
         //shape = new createjs.Shape();
@@ -101,8 +149,80 @@ Collatz = (function (createjs) {
         this.stage.update();
     };
 
-    
 
+    function Node(val){
+        var self = this;
+
+        self.value = val;
+        self.left = null;
+        self.right = null;
+        self.next = null;
+
+        self.isCalculated = false;
+
+    }
+
+    Node.prototype.calculate = function(nodes, allNodes){
+        if (!this.isCalculated){
+                var nextValue = this.value % 2 == 0 ? this.value / 2 : (this.value * 3 + 1);
+                var nextValueString = nextValue.toString();
+                var nextNode;
+
+                if (nextValueString in allNodes){
+                    nextNode = allNodes[nextValueString];
+                }
+                else{
+                    nextNode = new Node(nextValue);
+                }
+                this.next = nextNode;
+
+                if (this.value % 2 == 0){
+                    this.next.right = this;
+                }
+                else{
+                    this.next.left = this;
+                }
+                
+                this.isCalculated = true;
+
+                nextNode.calculate(nodes, allNodes);
+        }
+
+        Node.prototype.draw = function(shape, p, c){
+            if (!this.isDrawn){
+                this.isDrawn = true;
+                shape.graphics.lineTo(p.x, p.y);
+
+                if (this.left != null){
+
+                    var angle = p.angle - c.options.angle;
+
+                    var leftPoint = {
+                        x  : p.x + (c.options.length * Math.cos(degreesToRadians(angle))),
+                        y  : p.y + (c.options.length * Math.sin(degreesToRadians(angle))),
+                        angle : angle
+                    };
+
+                    var leftShape = c.createShape()
+                    leftShape.graphics.moveTo(p.x, p.y);
+
+                    this.left.draw(leftShape, leftPoint, c);
+                }
+
+                if (this.right != null){
+                    var angle = p.angle + c.options.angle;
+
+                    var rightPoint = {
+                        x  : p.x + (c.options.length * Math.cos(degreesToRadians(angle))),
+                        y  : p.y + (c.options.length * Math.sin(degreesToRadians(angle))),
+                        angle : angle
+                    };
+
+                    this.right.draw(shape, rightPoint, c);
+                }    
+            }
+        }
+    };
 
     return Collatz;
 })(createjs)
